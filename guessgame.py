644283090f4d1eb3618ab05d2a9c248821225005
jallen19 <jallen@stcharlessd.org>
2017-10-30 12:42:50 -0500

    generates target number

---
+++ guessgame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#guessgame.py
+#James Allen
+#10/30/17
+#simple number guessing game
+
+from random import randint
+MIN = 1
+MAX = 20
+
+
+targetNumber = randint(MIN,MAX)
+print(targetNumber)
